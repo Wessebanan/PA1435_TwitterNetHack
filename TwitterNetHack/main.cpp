352f1bc3319bd4770cc6b1540326c6d296f028fa
Robin Westerlund <robin_westerlund@hotmail.com>
2017-05-11 15:53:32 +0200

    wdiaksd

--- TwitterNetHack/main.cpp
+++ TwitterNetHack/main.cpp
@@ -1,10 +1,17 @@
 #include"Character.h"
 #include"Enemy.h"
 #include"Player.h"
+#include"Game.h"
+#include"TextHandler.h"
+#include"InputHandler.h"
 #include<iostream>
 using namespace std;
 int main()
 {
+	Game game;
+	TextHandler* txtH = new TextHandler();
+	Player player(txtH, 100, 20, 20);
+	Enemy enemy(txtH, 70, 10, 10, "Chestcunt", "It's a bitch boi");
 	bool running = true;
 	cout << "Welcome to TwitterNetHack!\n\n";
 	while (running)
@@ -15,6 +22,7 @@ int main()
 		cout << endl;
 		if (answer == "start")
 		{
+			game.battleSequence(&enemy);
 			//TODO: Start game.
 		}
 		else if (answer == "join")
