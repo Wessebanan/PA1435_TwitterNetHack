bccc18404434f3e855c1897791f31e3f098378c9
Rikard <magnomrikard@gmail.com>
2017-05-11 11:49:50 +0200

    Commiterino

--- TwitterNetHack/InputHandler.cpp
+++ TwitterNetHack/InputHandler.cpp
@@ -4,6 +4,10 @@ InputHandler::InputHandler(TextHandler * textHandler) {
 	this->textHandler = textHandler;
 }
 
+InputHandler::~InputHandler()
+{
+}
+
 void InputHandler::getInput() {
 	std::string input;
 	textHandler->printText(std::string("What do you want to do?"));
