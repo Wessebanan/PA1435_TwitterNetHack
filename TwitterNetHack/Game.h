439f9e83d10adf7e79dd0f4cc5fa8c2a96b7b546
Rikard <magnomrikard@gmail.com>
2017-05-11 12:24:37 +0200

    Gaimeh

--- TwitterNetHack/Game.h
+++ TwitterNetHack/Game.h
@@ -1,11 +1,20 @@
-
+#pragma once
+#include "Player.h"
+#include "Enemy.h"
+#include "InputHandler.h"
+#include "object.h" // Detta är Max inkompetens
+#include "TextHandler.h"
+#include "Room.h"
 
 
 class Game {
 private:
+	TextHandler textHandler;
+	InputHandler inputHandler;
+	Player player;
 
 
-
+	void generateMaze();
 
 public:
 	Game();
